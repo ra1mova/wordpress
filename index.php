d725c80694a13943cbe254f45d8a6ceab769393c
ra1mova <100186256+ra1mova@users.noreply.github.com>
2023-04-07 18:30:18 +0600

    hjkl

--- index.php
+++ index.php
@@ -3,9 +3,6 @@
  * Front to the WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load th
 
-/**
- * Tells WorbmndPress t
- */
 define( 'WP_USE_THEMES', true );
 
 /** Loads the WordPress Environment and Template */
