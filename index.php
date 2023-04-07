7f456cd45df347b827a64a9cd4c0071b5862713c
ra1mova <100186256+ra1mova@users.noreply.github.com>
2023-04-07 19:36:57 +0600

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /**
  * Front to the WordPress apasdfplication. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load th
-sdfasdfdsaf
+sdfasdfdsafasfddasf
 define( 'WP_USE_THEMES', true );
 
 /** Loads the WordPress Environment and Template */
